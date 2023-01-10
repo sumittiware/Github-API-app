5175802bdec2e5db3d46e2e5c8e9b4ac650717bc
Sumit Tiware <64718675+sumittiware@users.noreply.github.com>
2023-01-10 06:32:33 +0530

    Delete const.dart

--- lib/const.dart
+++
@@ -1,4 +0,0 @@
-const authToken =
-    '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';
-const apiServer = 'https://api.github.com';
-const repoName = 'freeCodeCamp';
